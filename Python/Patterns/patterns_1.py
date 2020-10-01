d72aa5ef5c43227fe4c3b47f50d31cb7f4cca557
kartikbisht806 <kartikbisht806@gmail.com>
2020-10-02 00:37:13 +0530

    added patterns_1.py file

---
+++ Python/Patterns/patterns_1.py
@@ -0,0 +1,18 @@
+n=int(input())
+i=1
+while i<=n:
+    j=1
+    while j<=i:
+        print("*", end=" ")
+        j+=1
+    print()
+    i+=1
+
+
+# input  : 5
+# output :
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
